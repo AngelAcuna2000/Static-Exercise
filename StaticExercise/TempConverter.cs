62d1af621d96a2598a4acff10e5b70673e8f998b
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-25 12:22:53 -0400

    Exercise Complete

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return 5.0 / 9.0 * (fahrenheit - 32.0);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return 9.0 / 5.0 * celsius + 32.0;
+        }
+    }
+}
